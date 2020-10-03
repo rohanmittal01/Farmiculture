fb9cd510e87bd34427c242d63557653709d5d00a
rohanmittal01 <rohanmittal01@gmail.com>
2020-10-03 09:32:32 +0530

    Added Backend Coand AI Modedde, odel Code

---
+++ Plant-recommendation-system/src/models/farmers.js
@@ -0,0 +1,92 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const Farmers = mongoose.model("Farmers", {
+  imageUrl: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  phoneNumber: {
+    type: Number,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  age: {
+      type: Number,
+      required: true
+  },
+  experience: {
+    type: Number,
+    required: true,
+  },
+  address: {
+    addressLine1: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    addressLine2: {
+      type: String,
+      trim: true,
+    },
+    city: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    state: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    country: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  price: {
+    type: Number,
+    required: true,
+    trim: true,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Price must be a positive integer");
+      }
+    },
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+  },
+  dateAdded: {
+    type: Date,
+    required: true,
+  },
+  addedBy: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  modifiedDate: {
+    type: Date,
+    required: true,
+  },
+  modifiedBy: {
+    type: String,
+    required: true,
+    trim: true,
+  }
+});
+
+module.exports = Farmers;
--- readme.md
+++ readme.md
@@ -9,3 +9,12 @@ Team 2 -
 <p align="center">
   <img max-width="100" height="100" src="https://github.com/rohanmittal01/Farmiculture/blob/master/readme_images/logo.png?raw=true">
 </p>
+
+# How to run the Project
+1. Change Directory to Farmiculture Folder.
+2. In Terminal, run the command 'npm install'
+3. In Terminal, execute 'ng serve'
+Now, the project must begin building and shall take some time.
+
+**Without the backend data, the AI Model will not work. But, the project design can be viewed Nonetheless.**
+****
